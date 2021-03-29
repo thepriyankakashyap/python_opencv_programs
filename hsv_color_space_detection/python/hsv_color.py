ea3905cca7915dcbb67a776ee2ba69f9b322390a
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2021-03-29 23:48:05 +0530

    Update hsv_color.py

--- hsv_color_space_detection/python/hsv_color.py
+++ hsv_color_space_detection/python/hsv_color.py
@@ -14,6 +14,24 @@ while True:
     red_mask = cv2.inRange(hsv_frame, low_red, high_red)
     red = cv2.bitwise_and(frame, frame, mask=red_mask)
     
+        # Blue color
+    low_blue = np.array([94, 80, 2])
+    high_blue = np.array([126, 255, 255])
+    blue_mask = cv2.inRange(hsv_frame, low_blue, high_blue)
+    blue = cv2.bitwise_and(frame, frame, mask=blue_mask)
+
+    # Green color
+    low_green = np.array([25, 52, 72])
+    high_green = np.array([102, 255, 255])
+    green_mask = cv2.inRange(hsv_frame, low_green, high_green)
+    green = cv2.bitwise_and(frame, frame, mask=green_mask)
+
+    # Every color except white
+    low = np.array([0, 42, 0])
+    high = np.array([179, 255, 255])
+    mask = cv2.inRange(hsv_frame, low, high)
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+    
         cv2.imshow("Frame", frame)
     cv2.imshow("Red", red)
     cv2.imshow("Blue", blue)
